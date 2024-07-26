176b9600b8309b7d46f2b58d53628e53af573b0d
ingridmonteiro993 <104505637+ingridmonteiro993@users.noreply.github.com>
2024-07-26 12:03:16 -0300

    f

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+alert('olá putadas');
+let numeroSecreto = 13;
 console.log('numeroSecreto')
 let chute = prompt('Escolha um número entre 1 e 100');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (7)');
+alert('Isso ai! Você descobriu o número secreto (13)');
 } else {
 alert('Você errou :(')
 }
